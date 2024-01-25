5056f850865bf92d3158f7a1fa029d65eacc35f7
TABLE3 <touloniut@gmail.com>
2024-01-25 11:23:15 +0100

    Finmessagerie deux PC

--- Courtois_Carpentier_WPF/MainWindow.xaml.cs
+++ Courtois_Carpentier_WPF/MainWindow.xaml.cs
@@ -13,6 +13,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ExtendedSerialPort_NS;
+using System.IO.Ports;
+using System.Windows.Threading;
+using System.Windows.Threading;
+
 
 namespace Courtois_Carpentier_WPF
 {
@@ -21,18 +26,45 @@ namespace Courtois_Carpentier_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        ExtendedSerialPort serialPort1;
+        DispatcherTimer timerAffichage;
+        string receivedText;
+
         public MainWindow()
         {
             InitializeComponent();
+            serialPort1 = new ExtendedSerialPort("COM21", 115200, Parity.None, 8, StopBits.One);
+            serialPort1.DataReceived += SerialPort1_DataReceived;
+            serialPort1.Open();
 
+            timerAffichage = new DispatcherTimer();
+            timerAffichage.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timerAffichage.Tick += TimerAffichage_Tick;
+            timerAffichage.Start();
+
+            
+        }
+
+        private void TimerAffichage_Tick(object? sender, EventArgs e)
+        {
+            textBoxReception.Text += receivedText;
+            receivedText = "";
+        }
+
+        public void SerialPort1_DataReceived(object sender, DataReceivedArgs e)
+        {
+            receivedText += Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+            
         }
 
         private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
         {
-            textBoxRéception.Text += "Reçu : " + textBoxEmission.Text + "\n";
-            textBoxEmission.Text = "";
-        }
+            textBoxReception.Text += "Reçu : " + textBoxEmission.Text + "\n"; 
+            serialPort1.WriteLine(textBoxEmission.Text);
 
+            textBoxEmission.Text = "";
+
+        }
 
         private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
         {
